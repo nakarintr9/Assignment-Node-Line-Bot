170400fd8871b1a891384fc690b47ce52c0e90a0
nakarintr9 <nakarintr9@gmail.com>
2020-05-20 00:19:50 +0700

    make it better 12

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 
 let channelAccessToken = 'XQ+Flk5yQfoS1X/FBErKW56urAMk5yFVRh1/7Y+L5fhHfAoPMWrxr1e4DI9R+Ln1Qis3vsKLAY9e8y7fL9Gn+PPkmCYPUY8PuGa/SOxdfflt0FSV61oQ4r40z+4JQQ6oVaNrOrOSEwtTTFbgo1Q7GgdB04t89/1O/w1cDnyilFU=';
 let channelSecret= '1d0c66301e63dda05ab05361844d7b62';
-// Echo reply
 
+// Reply using AIML, parsing data with AIMLParser
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const AIMLParser = require('aimlparser')
+
 const app = express()
 const port = process.env.PORT || 4000
+const aimlParser = new AIMLParser({ name:'HelloBot' })
+
+aimlParser.load(['./test-aiml.xml'])
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 app.post('/webhook', (req, res) => {
-    console.log(req.body);
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
+    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
+        reply(reply_token, answer)
+    })
     res.sendStatus(200)
 })
+
 app.listen(port)
+
 function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer '+channelAccessToken
     }
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
@@ -30,6 +41,7 @@ function reply(reply_token, msg) {
             text: msg
         }]
     })
+
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
